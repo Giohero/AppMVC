7c6ea10d4bd817b7c188f67120a3a0aed4a9e12d
Gio <Gio@PCMASTER_GIO>
2023-07-03 21:18:19 -0600

    Add project files.

---
+++ AppMVC/Controllers/AccesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace AppMVC.Controllers
+{
+    public class AccesController : Controller
+    {
+        // GET: Acces
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string User, string Password)
+        {
+            try
+            {
+                using (Models.EvicitiDBEntities db = new Models.EvicitiDBEntities())
+                {
+                    var oUser = (from d in db.Usuarios
+                                 where d.Nombre == User.Trim() && d.Contrasena == Password.Trim()
+                                 select d).FirstOrDefault();
+
+                    if (oUser == null)
+                    {
+                        ViewBag.Error = "Usuario o contraseña incorrectas";
+                        return View();
+                    }
+
+                    Session["User"] = oUser;
+
+                }
+                return RedirectToAction("Index", "Home");
+            }
+
+            catch (Exception ex)
+            {
+                ViewBag.Erro = ex.Message;
+                return View();
+            }
+
+        }
+    }
+}
---
+++ AppMVC/Filters/VerifiedSession.cs
@@ -0,0 +1,37 @@
+﻿using AppMVC.Controllers;
+using AppMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppMVC.Filters
+{
+    public class VerifiedSession: ActionFilterAttribute
+    {
+        private Usuarios oUsuario;
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            try
+            {
+                base.OnActionExecuting(filterContext);
+
+                oUsuario = (Usuarios)HttpContext.Current.Session["User"];
+                if(oUsuario == null)
+                {
+                    if(filterContext.Controller is AccesController == false)
+                    {
+                        filterContext.HttpContext.Response.Redirect("/Acces/Login");
+                    }
+                }
+
+            }
+            catch (Exception) 
+            {
+                filterContext.Result =new RedirectResult("~/Acces/Login");
+            }
+
+        }
+    }
+}
